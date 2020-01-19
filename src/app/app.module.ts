ed49104311f505b64682452c4bd0845af0ef8e05
iagosergiop@gmail.com <iagosergiop@gmail.com>
2020-01-19 09:03:48 -0800

    Footer creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DealComponent } from './components/deal/deal.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const routes: Route[] = [
+  { path: '', component: HomeComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DealComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
